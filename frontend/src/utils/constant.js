e3f7c8b71844a04ee804e0fe7faaceacd2430dfd
utkarshsinghakash <146868670+utkarshsinghakash@users.noreply.github.com>
2024-10-21 02:03:58 +0530

    api

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,7 @@
-export const USER_API_ENDPOINT = "http://localhost:3000/api/v1/user";
-export const JOB_API_ENDPOINT = "http://localhost:3000/api/v1/job";
-export const COMPANY_API_ENDPOINT = "http://localhost:3000/api/v1/company";
-export const APP_API_ENDPOINT = "http://localhost:3000/api/v1/application";
+export const USER_API_ENDPOINT =
+  "https://hirehub-ho32.onrender.com/api/v1/user";
+export const JOB_API_ENDPOINT = "https://hirehub-ho32.onrender.com/api/v1/job";
+export const COMPANY_API_ENDPOINT =
+  "https://hirehub-ho32.onrender.com/api/v1/company";
+export const APP_API_ENDPOINT =
+  "https://hirehub-ho32.onrender.com/api/v1/application";
