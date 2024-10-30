d3f9e424ab782a8152013da5f07d5ce1de2943ed
utkarshsinghakash <146868670+utkarshsinghakash@users.noreply.github.com>
2024-10-30 15:48:49 +0530

    logo

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -19,7 +19,7 @@ const isAuthenticated = async (req, res, next) => {
     req.id = decode.userId;
     next();
   } catch (err) {
-    console.log(err); // Fixed typo from "consoole" to "console"
+    console.log(err); 
   }
 };
 
